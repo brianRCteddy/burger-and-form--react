1e5460a4a9e414535ce660f95115333fddcc86ed
Teddy <brianrcdteodoro@gmail.com>
2019-12-11 15:56:33 +0800

    initial commit. Layout and BurgerBuilder Component

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Auxiliary from '../../hoc/Auxiliary';
+
+class BurgerBuilder extends Component {
+	render() {
+		return (
+			<Auxiliary>
+				<div>Final Burger</div>
+				<div>Builder Tools</div>
+			</Auxiliary>
+		);
+	}
+}
+export default BurgerBuilder;
