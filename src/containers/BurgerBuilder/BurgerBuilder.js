06b14e13358a1284ef1605a85d03312f2509337d
Teddy <brianrcdteodoro@gmail.com>
2019-12-11 17:55:41 +0800

    dynamic calling of ingredients from a state. transformed Objects to array

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
 import Auxiliary from '../../hoc/Auxiliary';
+import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component {
+	state = {
+		ingredients: {
+			salad: 1,
+			bacon: 1,
+			meat: 2,
+			cheese: 2
+		}
+	};
+
 	render() {
 		return (
 			<Auxiliary>
-				<div>Final Burger</div>
+				<Burger ingredients={this.state.ingredients} />
 				<div>Builder Controls</div>
 			</Auxiliary>
 		);
