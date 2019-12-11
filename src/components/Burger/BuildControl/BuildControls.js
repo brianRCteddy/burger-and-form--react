459267b71e3dacd772367ba1e8cc11bfd2cf2dbc
Teddy <brianrcdteodoro@gmail.com>
2019-12-11 22:39:16 +0800

    structured build controlss and build control component

---
+++ src/components/Burger/BuildControl/BuildControls.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import classes from './BuildControls.css';
+
+const BuildControls = (props) => {
+	return <div className={classes.BuildControls} />;
+};
+
+export default BuildControls;
