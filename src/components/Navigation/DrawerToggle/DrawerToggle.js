aaf49db414348e22d66bc090c050d5141b63af2b
Teddy <brianrcdteodoro@gmail.com>
2019-12-12 18:59:20 +0800

    added hamburger icon

--- src/components/Navigation/DrawerToggle/DrawerToggle.js
+++ src/components/Navigation/DrawerToggle/DrawerToggle.js
@@ -2,7 +2,7 @@ import React from 'react';
 import classes from './DrawerToggle.css';
 
 const DrawerToggle = (props) => (
-	<div classes={classes.DrawerToggle} onClick={props.toggleClicked}>
+	<div className={classes.DrawerToggle} onClick={props.toggleClicked}>
 		<div />
 		<div />
 		<div />
